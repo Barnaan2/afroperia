7f7bc7a7414b6977afb6ee82fb1e71e11d763a33
Barnaan <barnaan@gmail.com>
2022-08-06 12:07:16 +0300

    gitignore edited

--- Afroperia/settings.py
+++ Afroperia/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+
     'django.contrib.staticfiles',
     'hotel.apps.HotelConfig',
     'customer.apps.CustomerConfig',
